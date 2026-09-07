66921155fe5649c3d7469f44690fe72b835815aa
LauraEsmoris <lesmoris0web@gmail.com>
2026-09-07 11:38:56 -0500

    Cart page done

--- src/pageObjects/CartPage.ts
+++ src/pageObjects/CartPage.ts
@@ -1,6 +1,44 @@
+import { Locator, Page } from "playwright/test";
 
 
 
 export class CartPage {
-    
+
+    readonly page: Page;
+    readonly cartTitle: Locator;
+    readonly cartItem: Locator;
+    readonly productName: Locator;
+    readonly productPrice: Locator;
+    readonly productQuantity: Locator;
+    readonly removeButton: Locator;
+    readonly continueShoppingButton: Locator;
+    readonly checkoutButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.cartTitle = page.locator('[data-test="title"]');
+        this.cartItem = page.locator('[data-test="inventory-item"]');
+        this.productName = page.locator('[data-test="inventory-item-name"]');
+        this.productPrice = page.locator('[data-test="inventory-item-price"]');
+        this.productQuantity = page.locator('[data-test="item-quantity"]');
+        this.removeButton = page.locator('[data-test="remove-sauce-labs-backpack"]');
+        this.continueShoppingButton = page.locator('[data-test="continue-shopping"]');
+        this.checkoutButton = page.locator('[data-test="checkout"]');
+    }
+
+    async removeProduct() {
+        await this.removeButton.click();
+    }
+
+    async continueShopping() {
+        await this.continueShoppingButton.click();
+    }
+
+    async checkout() {
+        await this.checkoutButton.click();
+    }
+
+    async refresh() {
+        await this.page.reload();
+    }
 }
